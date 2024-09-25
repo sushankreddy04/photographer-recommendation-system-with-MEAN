4c54984daa8457fb21cfb4eb5105962bf2235fc5
sushankreddy04 <baddipadigesushank@gmail.com>
2024-09-25 20:44:25 +0530

    Initial commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -6,6 +6,10 @@ const homelist = (req, res) => {
    const locationInfo = (req, res) => {
     res.render('location-info', { title: 'Location info' });
    };
+   /* GET 'Location info2' page */
+   const locationInfo2 = (req, res) => {
+    res.render('location-info2', { title: 'Location info2' });
+   };
    /* GET 'Add review' page */
    const addReview = (req, res) => {
     res.render('location-review-form', { title: 'Add review' });
@@ -13,5 +17,6 @@ const homelist = (req, res) => {
    module.exports = {
     homelist,
     locationInfo,
+    locationInfo2,
     addReview
    };
